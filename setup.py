4166761ee04c5b9b596fc2446c0c89994d57b57a
Shankar <shankarviswanath14@gmail.com>
2025-07-19 16:33:30 +0530

    Folder Structure Updated with  init and setup.py also updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='document_portal',      
+    version='0.1',
+    packages=find_packages(),
+    author="Shankar Viswnath"
+)    
+
+    
