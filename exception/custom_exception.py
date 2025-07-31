cd9bd3c481d40456b5911e2e24c2cb5654a4453a
Shankar <shankarviswanath14@gmail.com>
2025-07-31 21:17:55 +0530

    dacument analyzer, prompt, model metadat format  updated

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -6,13 +6,13 @@ logger = CustomLogger().get_logger(__file__)
 class DocumentPortalException(Exception):
     """Custom exception for Document Portal"""
 
-    def __init__(self, error_message, error_details:sys):
-        print(error_details.exc_info())
-        _,_,exc_tb = error_details.exc_info()
+    def __init__(self, error_message):
+        print(sys.exc_info())
+        _,_,exc_tb = sys.exc_info()
         self.file_name = exc_tb.tb_frame.f_code.co_filename
         self.lineno=exc_tb.tb_lineno
         self.error_message = str(error_message)
-        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
+        self.traceback_str = ''.join(traceback.format_exception(*sys.exc_info()))
 
     def __str__(self):
         return f"""
@@ -29,6 +29,6 @@ if __name__ == "__main__":
         print(a)
 
     except Exception as e:
-        app_exc = DocumentPortalException(e, sys)
+        app_exc = DocumentPortalException(e)
         logger.error(app_exc)
         raise app_exc
